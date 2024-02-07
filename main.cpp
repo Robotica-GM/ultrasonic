4d15fd12aa47d1b9f793df70519c1b3dabe1332f
Francisco Juan <juansoussev@gmail.com>
2024-02-07 16:18:59 -0300

    [refactor] code optimization

--- main.cpp
+++ main.cpp
@@ -1,29 +1,34 @@
-#include <Ultrasonic.h>
- 
-//Define os pinos para o trigger e echo
-#define pino_trigger 4
-#define pino_echo 5
- 
-//Inicializa o sensor nos pinos definidos acima
-Ultrasonic ultrasonic(pino_trigger, pino_echo);
- 
-void setup()
-{
-  Serial.begin(9600);
-  Serial.println("Lendo dados do sensor...");
+const int trigPin = 5;
+const int echoPin = 6;
+long duration;
+float distanceCm;
+float distanceInch;
+unsigned long currentTime = millis();
+unsigned long previousTime = 0;
+const long timeoutTime = 2000;
+String distanciaCm;
+
+#define SOUND_SPEED 0.034
+
+void setup() {
+  Serial.begin(115200);      
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT);
 }
- 
-void loop()
-{
-  //Le as informacoes do sensor, em cm e pol
-  float cmMsec, inMsec;
-  long microsec = ultrasonic.timing();
-  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
-  inMsec = ultrasonic.convert(microsec, Ultrasonic::IN);
-  //Exibe informacoes no serial monitor
-  Serial.print("Distancia em cm: ");
-  Serial.print(cmMsec);
-  Serial.print(" - Distancia em polegadas: ");
-  Serial.println(inMsec);
+
+
+void loop() {
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distanceCm = duration * SOUND_SPEED/2;
+
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  
   delay(1000);
 }
